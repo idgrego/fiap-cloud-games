a11f8ac61677118d98295784268625482ba0ab3d
Ivo Gregorio <idgrego@msn.com>
2026-08-16 14:37:14 -0300

    Ativado o EnableRetryOnFailure para conexão com o banco de dados do Azure

--- fase-01/Program.cs
+++ fase-01/Program.cs
@@ -10,7 +10,36 @@ var builder = WebApplication.CreateBuilder(args);
 // Configurar o EF Core com a Connection String do Azure SQL
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer(connectionString, sqlOptions =>
+    {
+        sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 5,                  // Número máximo de tentativas
+            maxRetryDelay: TimeSpan.FromSeconds(10), // Intervalo entre tentativas
+        #region explicação do errorNumbersToAdd
+            // A propriedade errorNumbersToAdd serve para registrar 
+            // números de erros específicos do SQL Server aos quais 
+            // a política de tentativa automática (retry policy) 
+            // do Entity Framework Core deve reagir.
+
+            // Por padrão, quando você chama .EnableRetryOnFailure(), 
+            // o EF Core já possui uma lista interna pré-definida de códigos 
+            // de erro transitórios conhecidos do SQL Server 
+            // (por exemplo, erros de timeout de conexão, servidor ocupado, 
+            // perda momentânea de pacote, etc.).
+
+            // se a sua aplicação ou o seu banco no Azure SQL gerar um código 
+            // de erro customizado ou um erro específico que o EF Core não 
+            // trata por padrão como "transitório", você pode passá-lo na 
+            // propriedade errorNumbersToAdd.
+
+            // errorNumbersToAdd: new[] { 40613, 10928 } // Adiciona retentativas para erros específicos do Azure
+            // * 40613: Database is not currently available (quando o banco Azure SQL está em pausa/acordando).
+            // * 10928: Resource limit reached (limite de conexões ou DTUs atingido temporariamente no Azure).
+            // Se você não tiver erros específicos para adicionar, pode deixar como null
+        #endregion
+            errorNumbersToAdd: null);
+    })
+);
 
 // Add services to the container.
 builder.Services.AddScoped<IGamePhotoRepository, GamePhotoRepository>();
