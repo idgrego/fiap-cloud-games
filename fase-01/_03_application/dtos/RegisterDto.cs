8b620fa000fe4eacc1995f657b97b83dc4fd4bd8
Ivo Gregorio <idgrego@msn.com>
2026-08-26 14:41:41 -0300

    Configurando DataAnnotation para verificar o formato da senha

--- fase-01/_03_application/dtos/RegisterDto.cs
+++ fase-01/_03_application/dtos/RegisterDto.cs
@@ -22,7 +22,21 @@ namespace fase_01.application.dtos
         [Display(Name = "Password")]
         [Required(ErrorMessage = "The field {0} is required")]
         [DataType(DataType.Password)]
-        [MinLength(6, ErrorMessage = "The field {0} must be a minimum length of {1}")]
+        [RegularExpression(
+            @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$",
+            ErrorMessage = "The field {0} must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.")]
+        /// <summary>
+        /// User password. Must meet complexity requirements.
+        /// </summary>
+        /// <remarks>
+        /// Regex breakdown (^((?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$)):
+        /// - ^ : Start of text.
+        /// - (?=.*[a-z]) : At least 1 lowercase letter.
+        /// - (?=.*[A-Z]) : At least 1 uppercase letter.
+        /// - (?=.*\d) : At least 1 number.
+        /// - (?=.*[^\da-zA-Z]) : At least 1 special character.
+        /// - .{8,}$ : Minimum 8 characters in length.
+        /// </remarks>
         public string? Password { get; set; }
 
         [Display(Name = "Confirm Password")]
